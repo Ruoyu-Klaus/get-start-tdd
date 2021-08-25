98a639a70a3bc24404acc37eea45a7311d2da34e
RuoyuKlaus <klaus1201810802@gmail.com>
2021-08-25 13:07:27 +0800

    🎉 first setup github action with eslint

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-25 12:56:42
+ * @LastEditTime: 2021-08-25 12:56:43
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \cicd-github-action\index.js
+ */
+const attack = (name, damage) => {
+  return name + ` dealt ` + damage;
+};
