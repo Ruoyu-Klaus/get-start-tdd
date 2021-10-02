d76ab2dc773a20f22780e865347c8441e6778267
RuoyuWang <klaus1201810802@gmail.com>
2021-10-02 18:02:09 +0800

    ✅ TDD-3: passed all test

--- client/utiles/__test__/arrayify.test.js
+++ client/utiles/__test__/arrayify.test.js
@@ -8,3 +8,13 @@ it('returns an empty array when enter nothing', () => {
   const result = arrayify();
   expect(result).toEqual([]);
 });
+
+it('returns array with one element when enter a parameter', () => {
+  const result = arrayify('one');
+  expect(result).toEqual(['one']);
+});
+
+it('returns array of elements when enter a array parameter', () => {
+  const result = arrayify([1, 2, 3]);
+  expect(result).toEqual([1, 2, 3]);
+});
